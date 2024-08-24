e488d187d3266c3267fff002e3c7337947a90871
Paulo Salvatore <salvatoregames@gmail.com>
2024-08-23 21:20:10 -0300

    chore(app): add `.env` support & run `audit fix`

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProxyModule } from './proxy/proxy.module';
 import { AIModule } from '@/ai/ai.module';
-import {LandingPageModule} from "@/landing-page/landing-page.module";
+import { LandingPageModule } from '@/landing-page/landing-page.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,6 +15,9 @@ import {LandingPageModule} from "@/landing-page/landing-page.module";
 
     // AI
     AIModule,
+
+    // Config
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
 })
