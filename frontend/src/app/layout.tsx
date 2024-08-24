6a6681c05a9900d655cb121cd63b99032e2d4200
Paulo Salvatore <salvatoregames@gmail.com>
2024-08-23 22:51:35 -0300

    feat(frontend): install react query

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import React, { ReactNode } from 'react';
 import '../styles/global.css';
+import { Provider } from '@/src/app/Provider';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
