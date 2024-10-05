bf9e50131a6c718cb17fa3bcc2dd42d083600ad6
Paulo Salvatore <salvatoregames@gmail.com>
2024-10-04 21:16:53 -0300

    fix(wailist): add import to `MailerModule`

--- backend/src/waitlist/waitlist.module.ts
+++ backend/src/waitlist/waitlist.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WaitlistService } from './waitlist.service';
 import { WaitlistController } from './waitlist.controller';
+import { MailerModule } from '@/mailer/mailer.module';
 
 @Module({
+  imports: [MailerModule],
   controllers: [WaitlistController],
   providers: [WaitlistService],
   exports: [WaitlistService],
